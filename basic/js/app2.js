9f0fd96c488b1c395ada5b8b3d807d58ab231c40
nguyenchien <chiennguyen1702@gmail.com>
2022-07-08 22:24:49 +0700

    cc

--- basic/js/app2.js
+++ basic/js/app2.js
@@ -1,4 +1,10 @@
 var Validator = function (formElement) {
+  var formRule = {};
+
   var formElement = document.querySelector(formElement);
   var inputElements = formElement.querySelectorAll("input[name][rules]");
+  Array.from(inputElements).forEach(function (inputElement) {
+    formRule[inputElement.name] = inputElement.getAttribute("rules");
+  });
+  console.log(formRule);
 };
