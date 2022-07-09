840c7f1512a4084bdd6b8adf994bde5cb8243a9c
nguyenchien <chiennguyen1702@gmail.com>
2022-07-09 16:53:18 +0700

    cc

--- basic/js/app2.js
+++ basic/js/app2.js
@@ -1,10 +1,27 @@
 var Validator = function (formElement) {
   var formRule = {};
-
   var formElement = document.querySelector(formElement);
-  var inputElements = formElement.querySelectorAll("input[name][rules]");
-  Array.from(inputElements).forEach(function (inputElement) {
-    formRule[inputElement.name] = inputElement.getAttribute("rules");
-  });
-  console.log(formRule);
+
+  /*
+    Rule check:
+    Nếu có lỗi thì xuất ra message lỗi
+    Nếu không có lỗi thì xuất ra undefined
+  */
+  function validate(value) {
+    return value ? undefined : "Vui lòng nhập trường này.";
+  }
+
+  if (formElement) {
+    var inputElements = formElement.querySelectorAll("input[name][rules]");
+
+    Array.from(inputElements).forEach(function (inputElement) {
+      formRule[inputElement.name] = inputElement.getAttribute("rules");
+    });
+
+    Object.keys(formRule).forEach(function (inputElement) {
+      formElement.querySelector("#" + inputElement).onblur = function (e) {
+        console.log(validate(e.target.value));
+      };
+    });
+  }
 };
